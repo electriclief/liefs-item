a3d327f4bd98d4bcdcbfc13bc9532ec1e3c15068
Leafdriving <leafdriving@hotmail.com>
2017-01-29 00:37:50 -0500

    add _

---
+++ index_.js
@@ -0,0 +1,138 @@
+"use strict";
+var liefs_lib_1 = require("liefs-lib");
+var liefs_container_1 = require("liefs-container");
+var Item = (function () {
+    function Item(label, start, min, max, container) {
+        if (min === void 0) { min = undefined; }
+        if (max === void 0) { max = undefined; }
+        if (container === void 0) { container = undefined; }
+        this.label = label;
+        this.start = this.current = start;
+        if (min)
+            this.min = min;
+        if (max)
+            this.max = max;
+        if (container)
+            this.container = container;
+        if (!(label in Item.items))
+            Item.items[label] = [];
+        this.instance = Item.items[label].length;
+        Item.items[label].push(this);
+    }
+    Item.get = function (label, instance) {
+        if (instance === void 0) { instance = 0; }
+        if (label in Item.items)
+            return Item.items[label][instance];
+        return undefined;
+    };
+    Item.h = function () {
+        var Arguments = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            Arguments[_i] = arguments[_i];
+        }
+        Arguments.push("hor");
+        return I.apply(void 0, Arguments);
+    };
+    Item.v = function () {
+        var Arguments = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            Arguments[_i] = arguments[_i];
+        }
+        Arguments.push("ver");
+        return I.apply(void 0, Arguments);
+    };
+    Item.I = function () {
+        var Arguments = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            Arguments[_i] = arguments[_i];
+        }
+        var newItem;
+        var myArgsObj = liefs_lib_1.argsObj(arguments);
+        console.log(myArgsObj);
+        var Ilabel, Istart, Imin, Imax, Imargin;
+        var Iitems, Icontainer, IisHor;
+        var isItem;
+        var IpageTitle;
+        if ("array_Item" in myArgsObj) {
+            if (!("Item" in myArgsObj))
+                myArgsObj.Item = [];
+            for (var _a = 0, _b = myArgsObj["array_Item"]; _a < _b.length; _a++) {
+                var eachArray = _b[_a];
+                for (var _c = 0, eachArray_1 = eachArray; _c < eachArray_1.length; _c++) {
+                    var eachItem = eachArray_1[_c];
+                    myArgsObj.Item.push(eachItem);
+                }
+            }
+        }
+        if ("number" in myArgsObj)
+            Imargin = myArgsObj.number[0];
+        if ("string" in myArgsObj) {
+            for (var i = 0; i < myArgsObj.string.length; i++) {
+                isItem = myArgsObj.string[i];
+                if (isItem[0] === "-" || isItem[0] === "|") {
+                    IisHor = (isItem[0] === "-");
+                    myArgsObj.string[i] = isItem.slice(1);
+                }
+                if (isItem.slice(-1) === "-" || isItem.slice(-1) === "|") {
+                    IisHor = (isItem.slice(-1) === "-");
+                    myArgsObj.string[i] = isItem.slice(0, -1);
+                }
+                if (isItem.slice(0, 3) === "hor" || isItem.slice(0, 3) === "ver")
+                    IisHor = (isItem.slice(0, 3) === "hor");
+                else if (!(Ilabel))
+                    Ilabel = myArgsObj.string[i];
+                else if (!(IpageTitle))
+                    IpageTitle = myArgsObj.string[i];
+                if (isItem in Item.items) {
+                    if (!myArgsObj["item"])
+                        myArgsObj["item"] = [];
+                    myArgsObj["item"].push(items[isItem]);
+                }
+            }
+        }
+        if ("start" in myArgsObj) {
+            Istart = myArgsObj.start[0];
+            if (myArgsObj.start.length > 1)
+                Imin = myArgsObj.start[1];
+            if (myArgsObj.start.length > 2)
+                Imax = myArgsObj.start[2];
+            if (myArgsObj.start.length > 3)
+                liefs_lib_1.liefsError.badArgs("Start, Min, Max", "That, and more!", "Create Instance Of Item() " + JSON.stringify(myArgsObj.start.slice(3)));
+        }
+        if ("Item" in myArgsObj) {
+            Iitems = myArgsObj.Item;
+        }
+        if ("Container" in myArgsObj)
+            Icontainer = myArgsObj.container[0];
+        ///// ok now create
+        if (!Ilabel)
+            Ilabel = "item" + (Object.keys(Item.items).length / 1000).toFixed(3).slice(-3);
+        if (!Istart)
+            Istart = "0px"; // liefsError.badArgs("A Start Value", "none", "I() - " + Ilabel);
+        if (Iitems && Icontainer)
+            liefs_lib_1.liefsError.badArgs("items, or a container.", "received both", "Create Instance Of Item() " + Ilabel);
+        if (Iitems) {
+            if (IisHor === undefined) {
+                newItem = new Item(Ilabel, Istart, Imin, Imax);
+                newItem.pages = Iitems;
+                newItem.pages.unshift(newItem);
+                return newItem;
+            }
+            else
+                Icontainer = new liefs_container_1.Container(Ilabel, IisHor, Iitems, Imargin);
+        }
+        newItem = new Item(Ilabel, Istart, Imin, Imax, Icontainer);
+        if (IpageTitle)
+            newItem.pageTitle = IpageTitle;
+        return newItem;
+    };
+    Item.prototype.cssSelect = function () { return "#" + this.label; };
+    return Item;
+}());
+Item.debug = true;
+Item.items = {};
+var I = Item.I;
+var v = Item.v;
+var h = Item.h;
+var items = Item.items;
+var getItem = Item.get;
